6fd3e59843d9176e0e50810d83a849b12f354da7
Tushar Grover <tushargrover222004@gmail.com>
2025-02-16 20:11:22 +0530

    removed index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
     <Provider>    
       <App />
     </Provider>
-  // </StrictMode>,
+  </StrictMode>,
 )
